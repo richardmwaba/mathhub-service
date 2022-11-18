204de61a299d98781f13ec1030644ff1b4c6a34a
Richard Mwaba <ricch.mwaba@gmail.com>
2022-11-18 01:09:16 -0600

    Added service layer and DTO conversion for AssessmentType (#6)
    Co-authored-by: Richard <richard.w.mwaba@gmail.com>

--- src/main/java/com/hubformath/mathhubservice/services/config/IAssessmentTypeService.java
+++ src/main/java/com/hubformath/mathhubservice/services/config/IAssessmentTypeService.java
@@ -1,5 +1,17 @@
 package com.hubformath.mathhubservice.services.config;
 
+import java.util.List;
+
+import com.hubformath.mathhubservice.models.config.AssessmentType;
+
 public interface IAssessmentTypeService {
-    
+    public List<AssessmentType> getAllAssessmentTypes();
+
+    public AssessmentType createAssessmentType(AssessmentType assessmentType);
+
+    public AssessmentType getAssessmentTypeById(Long id);
+
+    public AssessmentType updateAssessmentType(Long id, AssessmentType assessmentType);
+
+    public void deleteAssessmentType(Long id);
 }
