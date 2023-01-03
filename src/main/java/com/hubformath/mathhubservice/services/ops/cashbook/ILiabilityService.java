a37da2b588b39c68b18dad8661f653c4c31f8e5b
Zaso <112427175+mwabazaso@users.noreply.github.com>
2023-01-03 03:58:51 +0200

    Added all the operations for Liability.

--- src/main/java/com/hubformath/mathhubservice/services/ops/cashbook/ILiabilityService.java
+++ src/main/java/com/hubformath/mathhubservice/services/ops/cashbook/ILiabilityService.java
@@ -1,5 +1,16 @@
 package com.hubformath.mathhubservice.services.ops.cashbook;
 
+import java.util.List;
+
+import com.hubformath.mathhubservice.models.ops.cashbook.Liability;
 public interface ILiabilityService {
-    
+    List<Liability> getAllLiabilities();
+
+    Liability createLiability(Liability liability);
+
+    Liability getLiabilityById(Long id);
+
+    Liability updateLiability(Long id, Liability liability);
+
+    void deleteLiability(Long id);
 }
--- src/main/java/com/hubformath/mathhubservice/services/ops/cashbook/impl/LiabilityServiceImpl.java
+++ src/main/java/com/hubformath/mathhubservice/services/ops/cashbook/impl/LiabilityServiceImpl.java
@@ -1,5 +1,66 @@
 package com.hubformath.mathhubservice.services.ops.cashbook.impl;
 
-public class LiabilityServiceImpl {
-    
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.hubformath.mathhubservice.models.ops.cashbook.Liability;
+import com.hubformath.mathhubservice.repositories.ops.cashbook.LiabilityRepository;
+import com.hubformath.mathhubservice.services.ops.cashbook.ILiabilityService;
+import com.hubformath.mathhubservice.utils.exceptions.ItemNotFoundException;
+
+@Service
+public class LiabilityServiceImpl implements ILiabilityService{
+    private final LiabilityRepository liabilityRepository;
+    private final String notFoundItemName;
+
+    public LiabilityServiceImpl(LiabilityRepository liabilityRepository) {
+        super();
+        this.liabilityRepository = liabilityRepository;
+        this.notFoundItemName = "liability";
+    }
+
+    @Override
+    public List<Liability> getAllLiabilities() {
+        return liabilityRepository.findAll();
+    }
+
+    @Override
+    public Liability getLiabilityById(Long id) {
+        Optional<Liability> liability = liabilityRepository.findById(id);
+
+        if(liability.isPresent()){
+            return liability.get();
+        } else {
+            throw new ItemNotFoundException(id, notFoundItemName);
+        }
+    }
+
+    @Override
+    public Liability createLiability(Liability liabilityRequest) {
+        return liabilityRepository.save(liabilityRequest);
+    }
+
+    @Override
+    public Liability updateLiability(Long id, Liability liabilityRequest) {
+        return liabilityRepository.findById(id)
+                .map(liability -> {
+                    liability.setPaymentMethod(liabilityRequest.getPaymentMethod());
+                    liability.setLiabilityType(liabilityRequest.getLiabilityType());
+                    liability.setAmount(liabilityRequest.getAmount());
+                    return liabilityRepository.save(liability);
+                })
+                .orElseThrow(() -> new ItemNotFoundException(id, notFoundItemName));
+    }
+
+    @Override
+    public void deleteLiability(Long id) {
+        Liability liability = liabilityRepository.findById(id)
+                .orElseThrow(() -> new ItemNotFoundException(id, notFoundItemName));
+
+        liabilityRepository.delete(liability);
+    }
 }
+
+    
