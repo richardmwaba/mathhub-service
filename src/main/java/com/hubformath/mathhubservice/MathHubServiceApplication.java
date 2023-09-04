f3a484b6a3e381eaf84b8ccf39a6ac4b679e2eec
Richard Mwaba <ricch.mwaba@gmail.com>
2023-09-04 14:18:56 -0500

    Added a temporal fix for CORS errors that show up when making requests from a browser (#40)
    * Uptick springboot version
    
    * Add invoice model in preparation for send invoice and code clean up
    
    * Change id type to UUID for config classes
    
    * Complete adding of UUID in all entities
    
    * Test invoices and fix bugs
    
    * Temporal cors error fix
    
    ---------
    
    Co-authored-by: Richard <richard.w.mwaba@gmail.com>

--- src/main/java/com/hubformath/mathhubservice/MathHubServiceApplication.java
+++ src/main/java/com/hubformath/mathhubservice/MathHubServiceApplication.java
@@ -3,7 +3,14 @@ package com.hubformath.mathhubservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class MathHubServiceApplication {
@@ -15,6 +22,22 @@ public class MathHubServiceApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MathHubServiceApplication.class, args);
+	}
+
+	// Fix the CORS errors: temporally
+	@Bean
+	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		// *** URL below needs to match the React client URL and port ***
+		config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 }
